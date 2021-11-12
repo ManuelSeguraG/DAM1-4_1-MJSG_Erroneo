8219759b757c9ebfd4f32d1fe572e24d427f4290
Manuel Segura <mjsegura@iesrafaelalberti.es>
2021-11-12 12:23:33 +0100

    Commit 4 Creacion y prueba de que el array bidimensional funciona

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -8,41 +8,93 @@ data class Alumno(
 class Modulo(
     var maxAlumnos: Int
 ) {
-    var arrayAlumnos = arrayOfNulls<Alumno>(maxAlumnos)
-    var arrayAlumnosOrdenados = arrayOfNulls<Alumno>(maxAlumnos)
-    var arrayNotas = Array(4) { arrayOfNulls<String>(maxAlumnos) }
+    var alumnos = arrayOfNulls<Alumno>(maxAlumnos)
+    var alumnosOrdenados = arrayOfNulls<Alumno>(maxAlumnos)
+    var evaluaciones = Array(4) { arrayOfNulls<String>(maxAlumnos) }
     var almacenIndex: Int = 0
 
     fun matricularAlumno(alumno: Alumno): Boolean {
-        when (arrayAlumnos.indexOfFirst { it == null }) {
-            in 0..maxAlumnos - 1 -> almacenIndex = arrayAlumnos.indexOfFirst { it == null }
+        when (alumnos.indexOfFirst { it == null }) {
+            in 0..maxAlumnos - 1 -> almacenIndex = alumnos.indexOfFirst { it == null }
             !in 0..maxAlumnos - 1 -> almacenIndex = -1
         }
         return if (almacenIndex.equals(-1)) {
             false
         } else {
-            arrayAlumnos[almacenIndex] = alumno
+            alumnos[almacenIndex] = alumno
             true
         }
 
     }
 
     fun bajaAlumno(idAlumno: String): Boolean {
-        when (arrayAlumnos.indexOfFirst { it?.id == idAlumno }) {
-            in 0..maxAlumnos - 1 -> almacenIndex = arrayAlumnos.indexOfFirst { it?.id == idAlumno }
+        when (alumnos.indexOfFirst { it?.id == idAlumno }) {
+            in 0..maxAlumnos - 1 -> almacenIndex = alumnos.indexOfFirst { it?.id == idAlumno }
             !in 0..maxAlumnos - 1 -> almacenIndex = -1
         }
         return if (almacenIndex.equals(-1)) {
             false
         } else {
-            arrayAlumnos[almacenIndex] = null
+            alumnos[almacenIndex] = null
             true
         }
 
     }
 
+    //SOLO PARA TESTEO
+    fun imprimirListaAlumnos() {
+        alumnosOrdenados = alumnos
+        alumnosOrdenados.sortWith(nullsLast(compareBy { it.id }))
+
+        println("--------------------------------")
+        println("LISTA ALUMNOS")
+        println("--------------------------------")
+        alumnosOrdenados.forEach { println("${it?.nombre} ${it?.ap1} || Identificador: ${it?.id}") }
+        println("--------------------------------")
+        println("")
+    }
+
+    fun establecerNotaV1() {
+        for (i in 0..maxAlumnos-1){
+            evaluaciones[0][i] = (0..10).random().toDouble().toString()
+            evaluaciones[1][i] = (0..10).random().toDouble().toString()
+            evaluaciones[2][i] = (0..10).random().toDouble().toString()
+            evaluaciones[3][i] = (0..10).random().toDouble().toString()
+        }
+    }
+
+    fun listaNotasV1(evaluacion: Int) {
+        if (evaluacion in 0..3) {
+            println("--------------------------------")
+            println("LISTA NOTAS DE LA EVALUACION ${evaluacion + 1}")
+            println("--------------------------------")
+            for (i in 0..maxAlumnos-1){
+                print("|${i + 1}: ${evaluaciones[0][i]}|")
+            }
+            println("")
+            println("--------------------------------")
+            println("")
+        } else {
+            println("Evaluacion no valida")
+        }
+    }
 }
 
 fun main() {
+    var alumno1 = Alumno("Juen", "Carglas", "Lunas", "1")
+    var alumno2 = Alumno("Pablo", "Carglas", "Lunas", "3")
+    var alumno3 = Alumno("Pedro", "Carglas", "Lunas", "2")
+
+    var moduloPROG = Modulo(20)
+
+    moduloPROG.matricularAlumno(alumno1)
+    moduloPROG.matricularAlumno(alumno2)
+    moduloPROG.matricularAlumno(alumno3)
+
+    moduloPROG.imprimirListaAlumnos()
+
+    moduloPROG.establecerNotaV1()
+
+    moduloPROG.listaNotasV1(1)
 
 }
